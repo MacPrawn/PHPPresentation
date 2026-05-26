a3da63a4cd10e9d66a5c93ce4f0b85b478dcd2c4
Jean Le Clerc <jean@nibnut.com>
2026-05-26 06:24:57 +0200

    Fixed bug where text runs without attributes would be skipped

--- src/PhpPresentation/Reader/PowerPoint2007.php
+++ src/PhpPresentation/Reader/PowerPoint2007.php
@@ -1510,10 +1510,10 @@ class PowerPoint2007 implements ReaderInterface
                 $oParagraph->createBreak();
             }
             if ('a:r' == $oSubElement->tagName) {
+                $oText = $oParagraph->createTextRun();
+                
                 $oElementrPr = $document->getElement('a:rPr', $oSubElement);
                 if (is_object($oElementrPr)) {
-                    $oText = $oParagraph->createTextRun();
-
                     if ($oElementrPr->hasAttribute('b')) {
                         $att = $oElementrPr->getAttribute('b');
                         $oText->getFont()->setBold('true' == $att || '1' == $att ? true : false);
@@ -1591,10 +1591,10 @@ class PowerPoint2007 implements ReaderInterface
                             $oText->getFont()->setCharset((int) $oElementFont->getAttribute('charset'));
                         }
                     }
-
-                    $oSubSubElement = $document->getElement('a:t', $oSubElement);
-                    $oText->setText($oSubSubElement->nodeValue);
                 }
+
+                $oSubSubElement = $document->getElement('a:t', $oSubElement);
+                $oText->setText($oSubSubElement->nodeValue);
             }
         }
     }
